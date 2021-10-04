0cd38b432368bd0beedf08f9369f63092fe4993a
vladuxa23 <vlad_1995.02@mail.ru>
2021-10-04 21:46:17 +0300

    some bugfix

--- app/handlers.py
+++ app/handlers.py
@@ -25,19 +25,18 @@ def get_summary(portfolio, portfolio_currency, price_currency):
             current_all_price = current_all_price * price_currency[one_lot_currency]
 
         portfolio_dict.update({position_name: {"value": balance,
-                                               "total_cost": current_all_price,
+                                               "total_cost": round(current_all_price, 2), # Подсчёт всего!!!
                                                "total_cost_currency": one_lot_currency,
-                                               "current_dynamic_price": current_dynamic_price,
+                                               "current_dynamic_price": round(current_dynamic_price, 2), #
                                                "current_dynamic_currency": current_dynamic_currency}})
         # print(f"{position_name}")
         # print(f"Всего:             {balance} лотов")
         # print(f"Общая цена:        {current_all_price} {one_lot_currency}")
         # print(f"С момента покупки: {current_dynamic_price} {current_dynamic_currency}")
 
-        if one_lot_currency == 'RUB':
-            portfolio_value += current_all_price
-        else:
-            portfolio_value += (current_all_price * price_currency[one_lot_currency])
+
+        portfolio_value += current_all_price
+
     portfolio_dict.update({"total_portfolio_cost": portfolio_value})
 
     return portfolio_dict
