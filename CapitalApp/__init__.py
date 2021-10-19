d53a012d6cf97b59344680f048dcc9be56ae16a0
vladuxa23 <vlad_1995.02@mail.ru>
2021-10-19 21:23:28 +0300

    add migration

--- CapitalApp/__init__.py
+++ CapitalApp/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 flask_app = Flask(__name__)
 flask_app.config.from_object('CapitalApp.configuration.Config')
 db = SQLAlchemy(flask_app)  # flask-sqlalchemy
+migrate = Migrate(flask_app, db)
 
 from CapitalApp import views, models
